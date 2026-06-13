03ea44b7221875dbdcb495da4e572e2838f493d8
Jamie Marie De Vera <128243233+jamiedevera@users.noreply.github.com>
2026-06-13 17:49:31 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/start-session",
+          destination: "/api/router.php",
+        },
+        {
+          source: "/m/:session*",
+          destination: "/api/router.php",
+        },
+        {
+          source: "/logout",
+          destination: "/api/router.php",
+        },
+        {
+          source: "/api/auth",
+          destination: "/api/router.php",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
